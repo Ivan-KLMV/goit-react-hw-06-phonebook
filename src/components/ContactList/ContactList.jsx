c1e148576857256da5dd257d0e9639d3af8ae90c
Ivan Klimov <klimov128@gmail.com>
2023-06-04 23:15:45 +0300

    Update ContactList.jsx
    add redux dispatch

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,24 +1,41 @@
-import PropTypes from 'prop-types';
 import { ContactListStyled } from './ContactList.styled';
+import { useDispatch, useSelector } from 'react-redux';
+import { deleteContact } from 'redux/contactListSlice';
 
-export const ContactList = ({ contacts, onClick }) => (
-  <ContactListStyled>
-    {contacts.map(contact => (
-      <li key={contact.id}>
-        {contact.name}:<span>{contact.number}</span>
-        <button
-          id={contact.id}
-          type="button"
-          onClick={() => onClick(contact.id)}
-        >
-          delete
-        </button>
-      </li>
-    ))}
-  </ContactListStyled>
-);
+export const ContactList = () => {
+  const dispatch = useDispatch();
+  const getContacts = state => state.contacts;
+  const getFilter = state => state.filter;
 
-ContactList.propTypes = {
-  contacts: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.string.isRequired)),
-  onClick: PropTypes.func,
+  const contacts = useSelector(getContacts);
+  const filter = useSelector(getFilter);
+
+  const handleFilterContact = () => {
+    const filterToLowerCase = filter.toLowerCase();
+
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filterToLowerCase)
+    );
+  };
+
+  const visibleContacts = handleFilterContact();
+
+  const handleDelete = id => dispatch(deleteContact(id));
+
+  return (
+    <ContactListStyled>
+      {visibleContacts.map(contact => (
+        <li key={contact.id}>
+          {contact.name}:<span>{contact.number}</span>
+          <button
+            id={contact.id}
+            type="button"
+            onClick={() => dispatch(handleDelete(contact.id))}
+          >
+            delete
+          </button>
+        </li>
+      ))}
+    </ContactListStyled>
+  );
 };
