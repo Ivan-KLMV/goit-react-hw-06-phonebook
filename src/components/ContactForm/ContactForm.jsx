1a49b88a32fad63c4b5bf7ef321c07c9886bd938
Ivan Klimov <klimov128@gmail.com>
2023-06-04 23:15:55 +0300

    Update ContactForm.jsx
    add redux dispatch

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,48 +1,30 @@
-import { useState } from 'react';
 import PropTypes from 'prop-types';
-import { nanoid } from 'nanoid';
 import { FormStyled } from './ContactForm.styled';
+import { useDispatch, useSelector } from 'react-redux';
+import { addContact } from 'redux/contactListSlice';
+import { nanoid } from '@reduxjs/toolkit';
 
-export const ContactForm = ({ contactsProp, onSubmitProp }) => {
-  const [name, setName] = useState('');
-  const [number, setNumber] = useState('');
+export const ContactForm = () => {
+  const dispatch = useDispatch();
 
-  const handleChange = e => {
-    switch (e.currentTarget.name) {
-      case 'name':
-        setName(e.target.value);
-        break;
-
-      case 'number':
-        setNumber(e.target.value);
-        break;
-
-      default:
-        break;
-    }
-  };
+  const getContacts = state => state.contacts;
+  const contacts = useSelector(getContacts);
 
   const handleSubmit = e => {
     e.preventDefault();
 
     const contact = {
       id: nanoid(),
-      name: name,
-      number: number,
+      name: e.target[0].value,
+      number: e.target[1].value,
     };
-
-    if (contactsProp.map(contact => contact.name).includes(contact.name)) {
+    if (contacts.map(contact => contact.name).includes(contact.name)) {
       return alert(`${contact.name} is already in contacts`);
     }
 
-    onSubmitProp(contact);
+    dispatch(addContact(contact));
+    e.target.reset();
     [...e.currentTarget].map(item => item.nodeName === 'INPUT' && item.blur());
-    reset();
-  };
-
-  const reset = () => {
-    setName('');
-    setNumber('');
   };
 
   return (
@@ -55,8 +37,8 @@ export const ContactForm = ({ contactsProp, onSubmitProp }) => {
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
-          onChange={handleChange}
-          value={name}
+          // onChange={handleChange}
+          // value={name}
         />
       </label>
       <label>
@@ -67,8 +49,8 @@ export const ContactForm = ({ contactsProp, onSubmitProp }) => {
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
-          onChange={handleChange}
-          value={number}
+          // onChange={handleChange}
+          // value={number}
         />
       </label>
       <button type="submit">add contact</button>
