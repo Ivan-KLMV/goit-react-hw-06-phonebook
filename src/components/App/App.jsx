1ddf58601fd282e437e469309228d922c2a7fb76
Ivan Klimov <klimov128@gmail.com>
2023-06-04 23:16:13 +0300

    Update App.jsx
    add redux

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,46 +1,23 @@
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import { ContactForm, ContactList, Filter } from '../index';
 import { AppContainer } from './App.styled';
+import { useSelector } from 'react-redux';
 
 export const App = () => {
-  const [contacts, setContacts] = useState(
-    () => JSON.parse(localStorage.getItem('contacts')) ?? []
-  );
-  const [filter, setFilter] = useState('');
+  const getContacts = state => state.contacts;
+  const contacts = useSelector(getContacts);
 
   useEffect(() => {
     localStorage.setItem('contacts', JSON.stringify(contacts));
   }, [contacts]);
 
-  const addContact = contact => {
-    setContacts(contacts => [contact, ...contacts]);
-  };
-
-  const filterHandler = e => {
-    setFilter(e.currentTarget.value);
-  };
-
-  const handleFilterContact = () => {
-    const filterToLowerCase = filter.toLowerCase();
-
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filterToLowerCase)
-    );
-  };
-
-  const deleteConact = id => {
-    setContacts([...contacts.filter(contact => contact.id !== id)]);
-  };
-
-  const visibleContacts = handleFilterContact();
-
   return (
     <AppContainer>
       <h1>phonebook</h1>
-      <ContactForm onSubmitProp={addContact} contactsProp={contacts} />
+      <ContactForm />
       <h2>contacts</h2>
-      <Filter filterValue={filter} onChange={filterHandler} />
-      <ContactList contacts={visibleContacts} onClick={deleteConact} />
+      <Filter />
+      <ContactList />
     </AppContainer>
   );
 };
